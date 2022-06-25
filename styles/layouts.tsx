5e305df70ec7e1bc980bb88845847762e89b2939
류호진 <interarea@naver.com>
2022-06-25 23:08:51 +0900

    update

---
+++ styles/layouts.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header``;
+
+const StyledMain = styled.main``;
+
+const StyledContainer = styled.div``;
+
+const StyledFooter = styled.footer``;
+
+export { StyledHeader, StyledMain, StyledContainer, StyledFooter};
